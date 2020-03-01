22a0ce80d7d2a0d792cfcfa0cf74d2b9a43f0a44
kulvirvirk <kvirkdev@gmail.com>
2020-03-01 07:20:10 +0000

    added requirements

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-a = int(input(3))
-b = int(input(2))
+# Task
+# Read two integers from STDIN and print three lines where:
 
-print(10**10)
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Input Format
+# The first line contains the first integer, . The second line contains the second integer, 
+
+# Constraints
+# a and b values are betten or equal to 1 to 10**10
+
+# Print the three lines as explained above.
+
+a = int(input('enter a:'))
+b = int(input('enter b:'))
+
+print(a + b)
+print(a-b)
+print(a*b)
